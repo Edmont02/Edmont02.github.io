d0c0a400ce686621f597566a2d151718d3802eb1
Edmont02 <142925292+Edmont02@users.noreply.github.com>
2024-07-27 17:58:42 -0400

    Update script.js
    Removed previous code and added navbar for header code for the menu button

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the default form submission
+let navbar = document.querySelector('.navbar');
 
-    // Collect form data
-    var formData = new FormData(this);
-
-    // Send email using EmailJS
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formData)
-        .then(function(response) {
-            alert('Message sent successfully!');
-        }, function(error) {
-            alert('Failed to send message: ' + JSON.stringify(error));
-        });
-});
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
