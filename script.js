e80daa5c43db71814b36a1587038c20997094477
Edmont02 <edmont02@louisville.edu>
2024-12-19 18:20:01 -0500

    Took away the option for light and dark mode and just left dark mode as the setting for the website. Also removed the toggle switch. Furthermore, added a description to home page.

--- script.js
+++ script.js
@@ -9,27 +9,3 @@ document.querySelector('#menu-btn').onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove('active');
 }
-
-// Theme toggle functionality
-const toggleSwitch = document.getElementById('theme-toggle');
-
-// Check if the user has a saved preference in localStorage
-if (localStorage.getItem('theme') === 'light') {
-    document.body.classList.add('light-mode');
-    toggleSwitch.checked = true;
-} else {
-    document.body.classList.add('dark-mode');
-}
-
-// Toggle theme when the switch is clicked
-toggleSwitch.addEventListener('change', () => {
-    if (toggleSwitch.checked) {
-        document.body.classList.remove('dark-mode');
-        document.body.classList.add('light-mode');
-        localStorage.setItem('theme', 'light');
-    } else {
-        document.body.classList.remove('light-mode');
-        document.body.classList.add('dark-mode');
-        localStorage.setItem('theme', 'dark');
-    }
-});
